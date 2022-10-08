4b0ab580b86903d6d9f04a49e0dcc0d5ee5a73dc
markoonit <markoonit008@gmail.com>
2022-10-08 07:41:39 +0200

    parsing date in the proper format

--- src/components/instalog/columns.js
+++ src/components/instalog/columns.js
@@ -1,4 +1,15 @@
 import LetterCircle from "../generic_components/LetterCircle";
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  var strTime = hours + ":" + minutes + " " + ampm;
+  return strTime;
+}
+
 export const columns = [
   {
     key: "icon",
@@ -17,7 +28,30 @@ export const columns = [
   {
     key: "occurred_at",
     parser: (value) => {
-      return value ? value.split("T")[0] : value;
+      let months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ];
+      let parsedDate = "";
+      if (value) {
+        value = new Date(value);
+        parsedDate += months[value.getMonth()];
+        parsedDate += " " + value.getDay() + ", ";
+        parsedDate += " " + formatAMPM(value);
+        return parsedDate;
+      } else {
+        return value;
+      }
     },
   },
 ];
